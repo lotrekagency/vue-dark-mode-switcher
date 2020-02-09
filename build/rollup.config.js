fa7b024c2280bb8e80f085e2f53f60a4b7a67faa
mino89 <pierdomenicoreitano@gmail.com>
2020-02-09 16:29:59 +0100

    added preview

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -2,7 +2,7 @@ import commonjs from 'rollup-plugin-commonjs'; // Convert CommonJS modules to ES
 import vue from 'rollup-plugin-vue'; // Handle .vue SFC files
 import buble from 'rollup-plugin-buble'; // Transpile/polyfill with reasonable browser support
 export default {
-    input: 'src/wrapper.js', // Path relative to package.json
+    input: 'src/components/wrapper.js', // Path relative to package.json
     output: {
         name: 'DarkModeSwitch',
         exports: 'named',
---
+++ src/App.vue
@@ -0,0 +1,122 @@
+<template>
+  <div id="app">
+    <div class="wrapper">
+      <!--  -->
+
+      <div class="canvas">
+        <div class="block day">
+          <div class="icon">🌞</div>
+        </div>
+        <div class="block night">
+          <div class="icon">🌜</div>
+        </div>
+      </div>
+      <div class="content">
+        <img class="logo" alt="Vue logo" src="./assets/logo.png" />
+        <h1>Vue Dark Mode Switcher</h1>
+        <DarkMode>
+          <div slot="darkIcon">
+            <button>Dark</button>
+          </div>
+          <div slot="lightIcon">
+            <button>Light</button>
+          </div>
+        </DarkMode>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import DarkMode from "./components/DarkMode";
+export default {
+  name: "App",
+  components: {
+    DarkMode
+  }
+};
+</script>
+
+<style lang="scss">
+@import url("https://cdn.jsdelivr.net/npm/reset-css@5.0.1/reset.min.css");
+
+body {
+  background: rgb(6, 0, 101);
+  background: linear-gradient(
+    180deg,
+    rgb(5, 25, 54) 0%,
+    rgb(133, 171, 252) 100%
+  );
+  background-size: 100% 200%;
+  background-position:center bottom;
+  transition: background 0.3s ease;
+  will-change: background;
+
+  &.theme-dark {
+    background-position: center top;
+    color: white;
+    .canvas {
+      transform: rotate(180deg);
+    }
+  }
+}
+
+.wrapper {
+  /* position: absolute; */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 100vh;
+  width: 100vw;
+  z-index: 1;
+  overflow: hidden;
+  position: relative;
+  .canvas {
+    z-index: -1;
+    position: absolute;
+    height: 200%;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    overflow: hidden;
+    transition: transform 0.3s ease;
+    will-change: transform;
+    top: 0;
+    .block {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 100vw;
+      height: 100vh;
+
+      &.day {
+      }
+      &.night {
+        transform: rotate(180deg);
+      }
+      .icon {
+        margin-top: -35%;
+
+        @media all and(max-width: 640px) {
+          margin-top: -80%;
+        }
+        font-size: 6rem;
+      }
+    }
+  }
+}
+
+.content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+
+.logo {
+  max-width: 4rem;
+}
+</style>
