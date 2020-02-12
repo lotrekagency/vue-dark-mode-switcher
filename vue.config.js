9a539f570053b7086e54a0d70f5a27af56afcf1f
mino89 <pierdomenicoreitano@gmail.com>
2020-02-12 23:51:02 +0100

    added some decorations

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,18 @@
 const path = require("path");
 
 module.exports = {
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule('svg');
+ 
+    svgRule.uses.clear();
+ 
+    svgRule
+      .use('babel-loader')
+      .loader('babel-loader')
+      .end()
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader');
+  },
   outputDir: path.resolve(__dirname, "dist/demo"),
   publicPath: process.env.NODE_ENV === 'production'
   ? '/vue-dark-mode-switcher/'
